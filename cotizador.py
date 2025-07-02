8d6e52ca96f76403bdde32a2eb90d8f2bc605d64
Santiplaza14 <santiaplaza14@gmail.com>
2025-07-02 16:25:18 -0300

    Add files via upload

---
+++ cotizador.py
@@ -0,0 +1,8 @@
+precio_lista = int(input("ingrese su precio de lista: "))
+precio_real = int(input("ingrese su precio modificado"))
+
+porcentaje_desc = ((precio_lista - precio_real) / precio_lista) * 100
+print(porcentaje_desc)
+
+
+
