c9f57822bd48a33e55528a5333c22cacb7cc2541
Santiplaza14 <santiaplaza14@gmail.com>
2025-07-02 16:55:35 -0300

    Update cotizador.py

--- cotizador.py
+++ cotizador.py
@@ -1,8 +1,11 @@
-precio_lista = int(input("ingrese su precio de lista: "))
-precio_real = int(input("ingrese su precio modificado"))
+import streamlit as st
 
-porcentaje_desc = ((precio_lista - precio_real) / precio_lista) * 100
-print(porcentaje_desc)
+st.title("Calculadora de Descuento (%)")
 
+precio_lista = st.number_input("Ingrese su precio de lista", min_value=0.0, format="%.2f")
+precio_real = st.number_input("Ingrese su precio modificado", min_value=0.0, format="%.2f")
 
+if precio_lista > 0:
+    porcentaje_desc = ((precio_lista - precio_real) / precio_lista) * 100
+    st.success(f"El descuento es: {porcentaje_desc:.2f}%")
 
